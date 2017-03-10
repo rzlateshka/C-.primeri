689743760a300f30f9809023e0807d8207debe66
Росица Златешка <r_zlateshka@yahoo.com>
2017-03-10 16:06:04 +0200

    Дефиниране на променливи и писане в конзолата

---
+++ Module 02/Module 02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Деклариране на променливи
+            int a = 0, b = 0;
+            int sum = 0;
+
+            // Събиране на числа
+            a = 50;
+            b = 6;
+            sum = a + b;
+
+            //Писане в конзолата
+            Console.Write(a);
+            Console.Write(" + ");
+            Console.Write(b);
+            Console.Write(" = ");
+            Console.WriteLine(sum);
+            Console.ReadKey();
+         }
+    }
+}
